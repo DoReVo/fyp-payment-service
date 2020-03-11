d491938e8d77eeff9c312a8eacd9953f10ad89d8
DoReVo <frz565@gmail.com>
2020-03-11 23:33:27 +0800

    STRIP Changed routing and handler

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\JWTHelper;
-use App\Payment;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
@@ -14,19 +12,14 @@ class PaymentController extends Controller
      *
      * @return void
      */
-    private $userId;
-    private $decodedToken;
+
     private $httpClient;
     public function __construct(Request $request)
     {
-        $jwtHandler = new JWTHelper;
         $token = $request->bearerToken();
-        $this->decodedToken = $jwtHandler->decode($token);
-        $this->userId = $this->decodedToken['uid'];
 
         $this->httpClient = new Client(
             [
-                'base_uri' => getenv('INVOICE_SERVICE_API'),
                 'headers' =>
                 [
                     // JWT token from this request
@@ -36,35 +29,16 @@ class PaymentController extends Controller
         );
     }
 
-    // params require:
-    // invoice_id
-    // amount
-    public function makePayment(Request $request)
+    public function handleRequest()
     {
-        // make payment
-        $payment = new Payment;
-
-        // get customer id from request bearer token or cookies
-        $payment->customer_id = $this->userId;
-        // invoice id from request body
-        $payment->invoice_id = $request->invoice_id;
-        // payment amount from request body
-        $payment->amount = $request->amount;
         try {
-            // save to payment record to db
-            $payment->save();
-        } catch (\Throwable $th) {
-            return response($th->getMessage(), 400);
-        }
-
-        // http request to tell invoice-service to update invoice status
-        try {
-            $invoice = $this->httpClient->patch('invoice/' . $request->invoice_id . '/pay', ['json' => ['payment_id' => $payment->id]]);
+            $url = getenv('INVOICE_SERVICE_API');
+            // http request to tell invoice-service to update invoice status
+            $invoice = $this->httpClient->get($url)->getBody();
+            return response($invoice, 200)->header('Content-Type', 'application/json');
         } catch (\Throwable $th) {
             return response()->json(array('error' => $th->getMessage()), 400);
         }
-
-        return response()->json($payment, 200);
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@
 |
  */
 
-$router->get('/', function () use ($router) {
-    return json_encode('This is payment-service');
-});
-
-$router->post('/payment', ['middleware' => 'JWTAuth', 'uses' => 'PaymentController@makePayment']);
+$router->get('/', ['middleware' => 'JWTAuth', 'uses' => 'PaymentController@handleRequest']);
